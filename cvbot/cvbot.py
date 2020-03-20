0977e02127a26d803513311e89f7639e238c6862
jsl12 <32917998+jsl12@users.noreply.github.com>
2020-03-20 17:36:55 -0500

    started with argparse, cases/deaths/recovered working for 1 country

--- cvbot/cvbot.py
+++ cvbot/cvbot.py
@@ -1,18 +1,13 @@
 import logging
-import re
 
 import discord
 
-from . import load, utils
+from . import load
+from .parser import CV_PARSER
 
 logger = logging.getLogger(__name__)
 
 class CoronaVirusBot:
-    REGEX = re.compile(f'(corona|virus)', re.IGNORECASE)
-    STAT_REGEX = re.compile(f'#([\w ,]+)-(deaths|cases|recovered)')
-    COMPARE_REGEX = re.compile(f'^#compare', re.IGNORECASE)
-    DOUBLE_REGEX = re.compile(f'^#double-([\w ,]+)', re.IGNORECASE)
-
     def __init__(self):
         self.client = discord.Client()
         return
@@ -21,50 +16,25 @@ class CoronaVirusBot:
         self.client.run(apikey)
 
     def process_message(self, msg: discord.Message):
-        match = self.STAT_REGEX.search(msg.content)
-        c_match = self.COMPARE_REGEX.search(msg.content)
-        d_match = self.DOUBLE_REGEX.search(msg.content)
-
-        if match is not None:
-            if match.group(2) == 'deaths':
-                stats = load.death_stats()
-            elif match.group(2) == 'cases':
-                stats = load.confirmed_stats()
-            elif match.group(2) == 'recovered':
-                stats = load.recovered_stats()
-
-            if msg.content[-6:] == 'series':
-                try:
-                    stats = stats[match.group(1)].sum(axis=1)
-                    stats = stats[stats > 0]
-                    return str(stats)[-2000:]
-                except Exception as e:
-                    logger.warning(f'Invalid key: {match.group(1)}')
-                    return
-
+        if self.client.user in msg.mentions:
             try:
-                stats = stats[match.group(1)]
-            except KeyError as e:
-                logging.info(f'trying to parse {match.group(1)} as a US state')
+                args = CV_PARSER.parse_args(msg.content.split(' ')[1:])
+            except Exception as e:
+                return repr(e)
+            else:
+                CMD_MAP = {
+                    'cases': load.confirmed_stats,
+                    'deaths': load.death_stats,
+                    'recovered': load.recovered_stats
+                }
+                df = CMD_MAP[args.command]()
 
                 try:
-                    stats = stats['US'][match.group(1)]
+                    res = df[args.country].sum(axis=1)
                 except KeyError as e:
-                    logger.warning(f'Invalid key: {match.group(1)}')
+                    return repr(e)
+
+                if args.series:
+                    return str(res)
                 else:
-                    return f'{stats.max().sum()}'
-            else:
-                return f'{stats.max().sum()}'
-
-        elif c_match is not None:
-            res = utils.compare(load.confirmed_stats(), msg.content[9:].split('-'))
-            res.index = res.index.to_series().apply(lambda t: f'{t.days} days')
-            return utils.df_to_str(res, 2)
-
-        elif d_match is not None:
-            res = utils.double_period(load.confirmed_stats()[d_match.group(1)].apply(sum, axis=1))
-            return str(res)
-
-        elif '#top10' in msg.content:
-            data = load.confirmed_stats().iloc[-1].sort_values(ascending=False).iloc[:10].reset_index(level=['Lat', 'Long'], drop=True)
-            return '\n'.join([f'{i[0]}, {i[1]} - {row}' for i, row in data.iteritems()]).replace(', nan', '')
+                    return res.values[-1]
---
+++ cvbot/parser.py
@@ -0,0 +1,35 @@
+import argparse
+
+"""
+@CVBot cases Italy
+@CVBot compare cases Italy US
+@CVBot series deaths China
+@CVBot series cases Iran
+
+cases Italy --compare US
+cases Italy --compare US --compare India
+deaths...
+recovered...
+"""
+
+
+class CVParseException(Exception):
+    pass
+
+
+class ErrorCatchingArgumentParser(argparse.ArgumentParser):
+    def exit(self, status=0, message=None):
+        if status:
+            raise CVParseException(''.join(message.split(':')[1:]).strip())
+        exit(status)
+
+
+CV_PARSER = ErrorCatchingArgumentParser()
+CV_PARSER.add_argument('command', type=str, help='Place to get stats from')
+CV_PARSER.add_argument('country', type=str, help='Country to get stats from')
+CV_PARSER.add_argument('-s', '--series', action='store_true', help='Return series')
+
+
+if __name__ == '__main__':
+    args = CV_PARSER.parse_args('cases Italy --series'.split(' '))
+    CV_PARSER.print_help()
